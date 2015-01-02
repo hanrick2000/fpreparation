da2d5a696081f40cf038944af12bff006cb365ed
Nikhil Katre <nkatre@ncsu.edu>
2015-01-02 18:43:23 -0500

    find powerset from set

---
+++ src/PowerSetFromSet/UsingBitIncrement.java
@@ -0,0 +1,33 @@
+package PowerSetFromSet;
+
+import java.util.Scanner;
+
+public class UsingBitIncrement {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	try{
+		System.out.println("Enter the string for powerset calculation");
+		String s = in.nextLine();
+		char[] set = s.toCharArray();
+		printPowerSet(set);
+	}
+	finally{
+		in.close();
+	}
+}
+
+private static void printPowerSet(char[] set) {
+	int totalElements = (int)Math.pow(2,set.length);
+	for(int i=0;i<totalElements;i++){
+		for(int j=0;j<set.length;j++){
+			if(((i) & (1<<j))!=0)   // For each bit of integer i, check which of the bits are set.
+				System.out.print(set[j]);  // If bit is set, then print the corresponding character
+			}
+		System.out.println();
+		}
+	}
+}
+/*
+Analysis:
+	Time Complexity = O(n*2^n)
+	Space Complexity = O(1)*/
