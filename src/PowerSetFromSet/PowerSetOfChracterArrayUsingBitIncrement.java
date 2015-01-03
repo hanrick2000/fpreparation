64f336a6b1466dd5dede73c843e0b8af0dae36cd
Nikhil Katre <nkatre@ncsu.edu>
2015-01-02 21:06:08 -0500

    added comment

--- src/PowerSetFromSet/PowerSetOfChracterArrayUsingBitIncrement.java
+++ src/PowerSetFromSet/PowerSetOfChracterArrayUsingBitIncrement.java
@@ -18,7 +18,7 @@ public static void main(String[] args) {
 
 private static void printPowerSet(char[] set) {
 	int totalElements = (int)Math.pow(2,set.length);
-	for(int i=0;i<totalElements;i++){
+	for(int i=0;i<totalElements;i++){    // i is the bitCounter which starts from 0000 and goes till 0111
 		for(int j=0;j<set.length;j++){
 			if(((i) & (1<<j))!=0)   // For each bit of integer i, check which of the bits are set.
 				// HandRun the above statement in if condition, to understand how to check for set bits in an integer
