cb7bf6885a26f8b942e30a7650b3f7e7db60dc01
Nikhil Katre <nkatre@ncsu.edu>
2015-01-05 04:16:54 -0500

    find whether a number is sum of two perfect squares using 2 for loops

---
+++ src/FindWhetherANumberIsSumOfPerfectSquares/UsingTwoForLoops.java
@@ -0,0 +1,35 @@
+package FindWhetherANumberIsSumOfPerfectSquares;
+
+import java.util.Scanner;
+
+public class UsingTwoForLoops {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	try{
+		System.out.println("Enter the number you need to check");
+		int n = in.nextInt();
+		sumOfPerfectSquares(n);
+	}
+	finally{
+		in.close();
+	}
+}
+
+private static void sumOfPerfectSquares(int n) {
+	int first = 0;
+	int second = 0;
+	for(int i=1;i<=n;i++){
+		first = i*i;
+		for(int j=2;j<=n;j++){
+			second = j*j;
+			if(first+second==n)
+				System.out.println("Pair is: "+i+" and "+j);
+		}
+	}
+}
+}
+/*
+Analysis:
+	Time Complexity= O(n^2)
+	Space Complexity= O(1)
+*/	
