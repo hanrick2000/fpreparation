6eec516e879812a08cc8e1ad22a5783a5d5e7da9
Nikhil Katre <nkatre@ncsu.edu>
2015-01-06 15:50:49 -0500

    added comments

--- src/lookAndSaySequence/UsingRunLengthEncoding.java
+++ src/lookAndSaySequence/UsingRunLengthEncoding.java
@@ -1,3 +1,35 @@
+/*
+Question:
+	Write a program to find pattern. 
+
+	0: 1 
+	1: 11 
+	2: 21 
+	3: 1211 
+	4: 111221 
+	5: 312211 
+
+	Iterate over the previous number, and find count for same number number. Append that count before number. 
+	
+	You're given number n and your task is to return nth look-and-say sequence.
+
+	Wiki:
+
+	In mathematics, the look-and-say sequence is the sequence of integers beginning as follows:
+
+	1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, ... (sequence A005150 in OEIS).
+	To generate a member of the sequence from the previous member, read off the digits of the previous member, 
+	counting the number of digits in groups of the same digit. For example:
+
+	1 is read off as "one 1" or 11.
+	11 is read off as "two 1s" or 21.
+	21 is read off as "one 2, then one 1" or 1211.
+	1211 is read off as "one 1, then one 2, then two 1s" or 111221.
+	111221 is read off as "three 1s, then two 2s, then one 1" or 312211.
+
+Source: http://blog.lckymn.com/2012/09/09/look-and-say-sequence/
+*/
+
 package lookAndSaySequence;
 
 import java.util.Scanner;
