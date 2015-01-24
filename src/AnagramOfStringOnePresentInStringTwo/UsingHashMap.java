836de455a242c851f46dec7121a2a02322077fbd
Nikhil Katre <nkatre@ncsu.edu>
2015-01-24 12:43:20 -0500

    anagrams of string one present in string two

--- src/AnagramOfStringOnePresentInStringTwo/UsingHashMap.java
+++ src/AnagramOfStringOnePresentInStringTwo/UsingHashMap.java
@@ -1,3 +1,40 @@
+
+
+/*
+ * Question: bool anaStrStr (string needle, string haystack) 
+{
+}
+Write a function that takes 2 strings , search returns true if any anagram of string1(needle)
+is present in string2(haystack)
+
+For Example: cat, actor -> T
+car, actor -> F
+
+Source: http://www.careercup.com/question?id=5671785349513216
+
+Algorithm: 
+The below solution is an O(n) time and O(n) space algorithm, although I assumed that the 2 strings can contain 
+any character (that is, the primitive type char in Java - 16-bit Unicode characters). We could conclude that it
+ is O(1) space also, because char can only have 65535 different values, therefore the Map's size is limited 
+ (has an upper bound). 
+
+1. We first build a map that contains the needle's characters and their number of occurences. 
+2. At this point we assume that the difference between the needle and the haystack is the length of
+ the needle, since we haven't read yet anything from the haystack. 
+3. We also create a map for the haystack (similar to that of the needle's map, but we will fill it
+ up along the way, it is empty first). 
+4. Then we iterate over the haystack and at every character we update the map of the haystack by looking 
+at the last n characters of the haystack (where n is the length of the needle). We can actually do this in 
+constant steps because we just remove an occurence of the character at the current-n position from the 
+haystack's map and add an occurence at the newly read position. Meanwhile, we keep track of the difference 
+between the needle and the current haystack "window". If at any point this difference is 0, then we return 
+true (the current part of the haystack is an anagram with the needle). Otherwise, if we reach the end of the 
+haystack without finding an anagram of the needle, then we return false. 
+
+It runs in O(n) time because we iterate over the haystack only once and at every step we do constant operations.
+ * 
+ */
+
 package AnagramOfStringOnePresentInStringTwo;
 
 import java.util.HashMap;
@@ -69,5 +106,5 @@ public static void main(String[] args) {
 /*
 Analysis:
 Time Complexity = O(n) where n = length of the largest string
-Space Complexity =O(n+m) where n = length of largest string, m = length of smallest string
+Space Complexity =O(n) where n = length of largest string
 */
