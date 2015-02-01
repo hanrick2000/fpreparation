4d3d2cd871e3d3610dfef8f91141d22fe3bdbd45
Nikhil Katre <nkatre@ncsu.edu>
2015-02-01 12:12:38 -0500

    made changes

--- src/NeedleHaystackProblemORAnagramOfOneStringPresentInSecondString/UsingPrimeNumbers_BESTALGO.java
+++ src/NeedleHaystackProblemORAnagramOfOneStringPresentInSecondString/UsingPrimeNumbers_BESTALGO.java
@@ -25,7 +25,7 @@ import java.util.Scanner;
 
 
 
-public class UsingPrimeNumbers {
+public class UsingPrimeNumbers_BESTALGO {
 
 public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
