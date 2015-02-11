3990b243d6a83006ff13dc5707bf8b9e9deb4f43
Nikhil Katre <nkatre@ncsu.edu>
2015-02-11 14:35:17 -0500

    PowerSet without Repetition

---
+++ src/PowerSetFromSet/PowerSetWithoutRepetition.java
@@ -0,0 +1,19 @@
+/*
+Question: We are given a set of integers with repeated occurences of elements.
+For Example, S={1,2,2}. 
+We need to print the power set of S ensuring that the repeated elements of the power
+set are printed only once. 
+For the above S, the power set will be 
+{NULL, {1}, {2}, {2}, {1,2}, {1,2}, {2,2}, {1,2,2}}. 
+So, as per the question requirements, we need to print 
+{NULL, {1}, {2}, {1,2}, {2,2}, {1,2,2}}
+
+Question Source: http://www.careercup.com/question?id=6189585818189824
+	
+Answer Source: 
+*/
+package PowerSetFromSet;
+
+public class PowerSetWithoutRepetition {
+
+}
