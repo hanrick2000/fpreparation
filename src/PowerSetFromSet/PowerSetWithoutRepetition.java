c886b9859ea1335440ffd0be6ebf024bf1e516b2
Nikhil Katre <nkatre@ncsu.edu>
2015-02-11 15:15:55 -0500

    powerset without repetition

--- src/PowerSetFromSet/PowerSetWithoutRepetition.java
+++ src/PowerSetFromSet/PowerSetWithoutRepetition.java
@@ -8,12 +8,79 @@ For the above S, the power set will be
 So, as per the question requirements, we need to print 
 {NULL, {1}, {2}, {1,2}, {2,2}, {1,2,2}}
 
-Question Source: http://www.careercup.com/question?id=6189585818189824
-	
-Answer Source: 
+Question And Answer Source: http://www.careercup.com/question?id=6189585818189824
 */
 package PowerSetFromSet;
 
-public class PowerSetWithoutRepetition {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class PowerSetWithoutRepetition {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		try{
+			System.out.println("Program which prints the power set of a set which contains REPEATED elements BUT does not "
+					+ "repeat repetitions in the powerset");
+			System.out.println("Enter the number of elements in the array list");
+			int n = in.nextInt();
+			System.out.println("Enter the integers with duplicates");
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			for(int i=0;i<n;i++)
+				list.add(in.nextInt());
+			powerSet(list);
+		}
+		finally{
+			in.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void powerSet(ArrayList<Integer> input) {
+		ArrayList<ArrayList<Integer>> powerSet = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> set = new ArrayList<Integer>();
+		powerSet.add(set);
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		int focoused;
+		int counter=0,dupCounter;
+		if (input.isEmpty())
+			System.out.println("null");
+		else {
+			while (!input.isEmpty()) {
+				focoused = input.remove(0);
+				int psize = powerSet.size();
+				counter=0;
+				dupCounter=0;
+				{
+					if(hm.containsKey(focoused))
+					{
+						hm.put(focoused, hm.get(focoused)+1);
+						dupCounter = hm.get(focoused);
+						for (int i = 0; i < psize; i++) {
+							for(Integer ii : powerSet.get(i))
+								if(ii==focoused)
+									counter++;
+							if(counter==dupCounter-1)
+							{
+								powerSet.get(i).contains(focoused);
+								set = (ArrayList<Integer>) powerSet.get(i).clone();
+								set.add(focoused);
+								powerSet.add(set);
+							}
+							counter=0;
+						}
+					}
+					else{
+						hm.put(focoused, 1);
+						for (int i = 0; i < psize; i++) {
+							set = (ArrayList<Integer>) powerSet.get(i).clone();
+							set.add(focoused);
+							powerSet.add(set);
+						}
+					}
+				}
+			}
+			System.out.println(powerSet.toString());
+		}
+	}
 }
