cc59244e295c19355ca80f6dbe8ff2dc3d73e4a1
Nikhil Katre <nkatre@ncsu.edu>
2015-02-11 19:56:42 -0500

    new file

---
+++ src/Array/UniqueThreeMemberSubset/UsingAlgorithm.java
@@ -0,0 +1,11 @@
+/*
+Question: Given an array, find all unique three-member subsets, 
+with unique being that [0,2,3] and [3,2,0] are the same set. Should run in faster than 2^n time
+
+Question and Answer Source: http://www.careercup.com/question?id=16760663
+*/
+package Array.UniqueThreeMemberSubset;
+
+public class UsingAlgorithm {
+
+}
