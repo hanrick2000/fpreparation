904e45f1a4cc977fae99f9f0b35f0f94b66f7467
Nikhil Katre <nkatre@ncsu.edu>
2015-02-20 15:17:08 -0500

    online users

---
+++ src/OnlineUsers/FindOnlineUsersFromLoginAndLogoutTimes.java
@@ -0,0 +1,15 @@
+/*
+Question: A period of time where users login and logout, given a sets of login and logout 
+time pairs, write a function that can show the number of users online at any given time.
+
+Question Source: http://www.careercup.com/question?id=14797777
+	
+IMP Sources: 
+	http://www.careercup.com/question?id=14797777
+	http://www.careercup.com/question?id=5979695413723136
+*/
+package OnlineUsers;
+
+public class FindOnlineUsersFromLoginAndLogoutTimes {
+
+}
