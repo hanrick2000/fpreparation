fed6cc4710514df0ff1e62d9b3074f158e31e96e
Nikhil Katre <nkatre@ncsu.edu>
2015-02-25 13:22:42 -0500

    added some files

--- src/Intervals/MinMeetingRooms/UsingGreedyApproach.java
+++ src/Intervals/MinMeetingRooms/UsingGreedyApproach.java
@@ -6,6 +6,7 @@ Question: You have a number of meetings (with their start and end times).
 Question Source: Facebook Interview Question
 
 IMP Sources:
+http://stackoverflow.com/questions/24657695/optimal-room-count-and-sizes-for-n-overlapping-meeting-schedules
 	https://nuttynanaus.wordpress.com/2014/04/26/software-engineer-interview-questions-3/
 		https://hellosmallworld123.wordpress.com/2014/05/30/arranging-the-meeting-room/
 */
