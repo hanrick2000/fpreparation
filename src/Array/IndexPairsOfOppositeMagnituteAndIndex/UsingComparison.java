d73f626683a2c8d4fc7087b7fb914208bccae3ac
Nikhil Katre <nkatre@ncsu.edu>
2015-03-03 21:33:41 -0500

    added content

--- src/Array/IndexPairsOfOppositeMagnituteAndIndex/UsingComparison.java
+++ src/Array/IndexPairsOfOppositeMagnituteAndIndex/UsingComparison.java
@@ -6,7 +6,15 @@ Question: You are given two integer arrays A and B .
 
 find all the pairs(i,j) such that : i < j and A[i]>B[j]
 		
-Question Source: http://www.careercup.com/question?id=6260358392053760
+Question and Solution Source: http://www.careercup.com/question?id=6260358392053760
+
+
+The problem is basically counting number of inversions in an array. 
+Use modified merge sort. 
+1.combine A and B arrays into C. 
+2.label each element in C with either A or B. 
+3.Count number of inversion pairs using merge sort. 
+4. For each inversion pair , reject if label(i) == label(j)
 
 */
 package Array.IndexPairsOfOppositeMagnituteAndIndex;
@@ -30,14 +38,14 @@ public static void main(String[] args) {
 		for(int i=0;i<n;i++)
 			b[i]=in.nextInt();
 		System.out.println("Pairs such that i<j and a[i]>b[j] are: ");
-		oppPairs(a,b);
+		bruteForce(a,b);
 	}
 	finally{
 		in.close();
 	}
 }
 
-private static void oppPairs(int[] a, int[] b) {
+private static void bruteForce(int[] a, int[] b) {
 	for(int i=0;i<a.length;i++){
 		for(int j=i+1;j<b.length;j++){
 			if(a[i]>b[j])
@@ -45,7 +53,10 @@ private static void oppPairs(int[] a, int[] b) {
 		}
 	}
 }
+ // TODO LEFT
+
 }
+
 /*
 Analysis:
 	Time Complexity = O(m*n)
@@ -59,7 +70,7 @@ worst case [where O(N^2) pairs has to be reported]?
 2) Tell the interviewer that we're engineers and we are going to solve this no matter what. Show what 
 constraints we can change to provide O(N) time complexity or better. 
 For instance we can escape from classical sequential model and design a system (either design a new
-ardware system or build a distributed systems with many processors/computers) - where we can leverage 
+hardware system or build a distributed systems with many processors/computers) - where we can leverage 
 O(N) processors with very efficient communication between each pair of processors. In this case O(N) solution 
 is possible. We can find O(N^2) pairs and report them in O(N) time into a distributed storage across our O(N) nodes. 
 If I was an interviewer at Amazon I'd be happy to hear such answer.
