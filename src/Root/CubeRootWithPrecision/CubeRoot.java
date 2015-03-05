8d19d7afa03eba1f523210d2a0ca5b4a2180e126
Nikhil Katre <nkatre@ncsu.edu>
2015-03-05 10:53:59 -0500

    added content

--- src/Root/CubeRootWithPrecision/CubeRoot.java
+++ src/Root/CubeRootWithPrecision/CubeRoot.java
@@ -4,6 +4,7 @@
  * Question: Find the cube root of a number
  * 
  * Source: http://codinggeeks.blogspot.com/2010/04/computing-square-cube-roots.html
+ * http://codinggeeks.blogspot.com/2010/04/computing-derivatives.html
  * 
  */
 
