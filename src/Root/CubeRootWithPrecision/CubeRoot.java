ce1f850ae6a5fcce9c8555d48c604dea56790129
Nikhil Katre <nkatre@ncsu.edu>
2015-03-05 17:11:15 -0500

    added content

--- src/Root/CubeRootWithPrecision/CubeRoot.java
+++ src/Root/CubeRootWithPrecision/CubeRoot.java
@@ -11,6 +11,43 @@
 
 package Root.CubeRootWithPrecision;
 
-public class CubeRoot {
+import java.util.Scanner;
 
+public class CubeRoot {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		try{
+			System.out.println("Program to find the CUBE ROOT OF A NUMBER WITH PRECSION IN FAST TIME");
+			System.out.println("Enter the number");
+			int n = in.nextInt();
+			System.out.println("The cube root of the number is: "+cubeRoot(n));
+		}
+		finally{
+			in.close();
+		}
+	}
+	public static int cubeRoot(int n) {
+
+		int result=0;
+		for (int i=1;i<n;i++){
+			
+			if (i*i*i == n){
+				result =i;
+				break;
+			}
+			if (i*i*i>n){
+				result = i-1;
+				break;
+			}
+		}
+		
+		return result; 
+
+
+	}
+	/*
+	 * Analysis:
+	 * Time complexity = O()
+	 * Space Complexity = O(1)
+	 */
 }
