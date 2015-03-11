56bf9caad62942b86a70f6ddd293b90cfb8078bf
Nikhil Katre <nkatre@ncsu.edu>
2015-03-10 23:12:37 -0400

    added content

--- src/FlattenNestedDatastructures/FlattenNestedList.java
+++ src/FlattenNestedDatastructures/FlattenNestedList.java
@@ -56,8 +56,6 @@ public class FlattenNestedList implements Iterator { // VERY VERY IMP: implement
 
 	public static void main(String[] args) {
 	
-		
-		
 		ArrayList<Integer> l1 = new ArrayList<Integer>();
 		ArrayList<Integer> l2 = new ArrayList<Integer>();
 		ArrayList<Integer> l3 = new ArrayList<Integer>();
