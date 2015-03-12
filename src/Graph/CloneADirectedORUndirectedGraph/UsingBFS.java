7ae4a0b375aee790a285648f35742dc717efd79c
Nikhil Katre <nkatre@ncsu.edu>
2015-03-12 07:29:54 -0400

    added content

--- src/Graph/CloneADirectedORUndirectedGraph/UsingBFS.java
+++ src/Graph/CloneADirectedORUndirectedGraph/UsingBFS.java
@@ -19,50 +19,59 @@ import java.util.Queue;
  */
 /**	
  * Definition for undirected graph.
- * class UndirectedGraphNode {
+ * class GraphNode {
  *     int label;
- *     ArrayList<UndirectedGraphNode> neighbors;
- *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ *     ArrayList<GraphNode> neighbors;
+ *     GraphNode(int x) { label = x; neighbors = new ArrayList<GraphNode>(); }
  * };
  */
 public class UsingBFS {
 		
 	// IMP: http://www.programcreek.com/2012/12/leetcode-clone-graph-java/
 	
-    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    public GraphNode cloneGraph(GraphNode node) {
         if(node == null)
             return null;
         
-        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
-        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode,UndirectedGraphNode>();
-        UndirectedGraphNode newHead = new UndirectedGraphNode(node.label);              // CLONE
+        Queue<GraphNode> queue = new LinkedList<GraphNode>();
+        HashMap<GraphNode, GraphNode> map = new HashMap<GraphNode,GraphNode>();
         
+        // CAM = THREE IMP POINTS (Clone, Add, Map)
+        GraphNode newHead = new GraphNode(node.label); // CLONE
         queue.add(node);          // ADD
         map.put(node, newHead);   // MAP
  
         while(!queue.isEmpty()){
-            UndirectedGraphNode curr = queue.poll();  // remove the node
-            ArrayList<UndirectedGraphNode> currNeighbors = curr.neighbors; // get the neighbors 
-            for(UndirectedGraphNode aNeighbor: currNeighbors){
-                if(!map.containsKey(aNeighbor)){
-                    UndirectedGraphNode copy = new UndirectedGraphNode(aNeighbor.label); // CLONE
+            GraphNode curr = queue.remove();  // remove the node
+            ArrayList<GraphNode> currNeighbors = curr.neighbors; // get the neighbors 
+            
+            for(GraphNode aNeighbor: currNeighbors){
+               
+            	
+            	if(!map.containsKey(aNeighbor)){
+            		// CAM = THREE IMP POINTS (Clone, Add, Map)
+                    GraphNode copy = new GraphNode(aNeighbor.label); // CLONE
                     queue.add(aNeighbor);         // ADD
                     map.put(aNeighbor,copy);      // MAP
+                    // Add cloned neighbor to cloned node
                     map.get(curr).neighbors.add(copy);     // this is same as the below line "map.get(curr).neighbors.add(map.get(aNeighbor))"
                 }
                 else{
                     map.get(curr).neighbors.add(map.get(aNeighbor));
                 }
+            	
+            	
             }
         }
         return newHead;
     }
 }
-class UndirectedGraphNode {
+class GraphNode {
 	      int label;
-	      ArrayList<UndirectedGraphNode> neighbors;
-	      UndirectedGraphNode(int x) {
+	      ArrayList<GraphNode> neighbors;
+	      
+	      public GraphNode(int x) {
 	    	  label = x;
-	    	  neighbors = new ArrayList<UndirectedGraphNode>(); 
-	    	  }
+	    	  neighbors = new ArrayList<GraphNode>(); 
+	      }
 	 }
