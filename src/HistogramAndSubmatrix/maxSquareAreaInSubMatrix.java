e0859a164e95d7477a89ace1ada908a6a0f525b4
Nikhil Katre <nkatre@ncsu.edu>
2015-03-12 18:12:45 -0400

    added content

--- src/HistogramAndSubmatrix/maxSquareAreaInSubMatrix.java
+++ src/HistogramAndSubmatrix/maxSquareAreaInSubMatrix.java
@@ -2,7 +2,7 @@
 Question: Find Maximum size square sub-matrix with all 1's
 Source: 	
 Algorithm:
-	1. create a solution matrix 
+	1. create a AUX matrix 
 	2. Copy the 0th row as it is
 	3. Copy the 0th column as it is
 	4. iterate from 1st row and 1st column till the end
@@ -29,24 +29,24 @@ class maxSquareAreaInSubMatrix{
 	                                    
 	        System.out.println("The max area is: "+findLargest(knows));
 	    }
-	    public static int findLargest(int[][] a){
+	    public static int findLargest(int[][] original){
 
-	        if(a==null||a.length==0)
+	        if(original==null||original.length==0)
 	            return -1;
 	        
-	        int[][] aux = new int[a.length][a[0].length];
+	        int[][] aux = new int[original.length][original[0].length];
 	        int max = 0;
 	        
-	        for(int i=0;i<a.length;i++)
-	            aux[i][0] = a[i][0];
+	        for(int i=0;i<original.length;i++)
+	            aux[i][0] = original[i][0];
 	            
-	        for(int j=0;j<a[0].length;j++)
-	            aux[0][j] = a[0][j];
+	        for(int j=0;j<original[0].length;j++)
+	            aux[0][j] = original[0][j];
 	        
-	        for(int i=1;i<a.length;i++){
-	            for(int j=1;j<a[0].length;j++){
-	                if(a[i][j]==1)
-	                	aux[i][j] = Math.min(a[i-1][j-1],Math.min(a[i-1][j],a[i][j-1]))+1; // Math.min(input[i][j-1],input[i-1][j],input[i-1][j-1])+1
+	        for(int i=1;i<original.length;i++){
+	            for(int j=1;j<original[0].length;j++){
+	                if(original[i][j]==1)
+	                	aux[i][j] = Math.min(original[i-1][j-1],Math.min(original[i-1][j],original[i][j-1]))+1; // Math.min though we are finding max area VERY VERY IMP
 	                else
 	                	aux[i][j]=0;
 	                max=Math.max(aux[i][j], max);
@@ -60,7 +60,7 @@ class maxSquareAreaInSubMatrix{
 	}
 /*
  * Analaysis:
- * 			Time Complexity = O(m*n) where m = number of rows
+ * 			Time Complexity = O(m*n) where original = number of rows
  * 										   n = number of columns
  * 			Space Complexity = O(m*n)
 */
