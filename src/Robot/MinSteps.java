7dcbaca95a7178d7204f8ff772f6329bce8552dc
Nikhil Katre <nkatre@ncsu.edu>
2015-03-14 00:09:22 -0400

    added content

--- src/Robot/MinSteps.java
+++ src/Robot/MinSteps.java
@@ -3,6 +3,9 @@ Question: A robot has to move in a grid which is in the form of a matrix. It can
 1.) A(i,j)--> A(i+j,j) (Down) 
 2.) A(i,j)--> A(i,i+j) (Right) 
 
+VERY VERY IMP NOTE: This question is different from uniquePaths question in the sense that 
+  * in that question the robot can move (i+1,j) and (i,j+1) BUT here it is (i+j,j) and (i,i+j)
+
 Given it starts at (1,1) and it has to go to A(m,n), find the minimum number of STEPS it has to take to get to (m,n) and write 
 public static int minSteps(int m,int n) 
 
@@ -45,89 +48,4 @@ public class MinSteps {
 			return -1;
 	}
 	
-	 public static int uniquePaths ( int m, int n) {
-         int[][] dp =new int[m][n];
-         // initialize dp, mx 1 for 1 whole situation 
-        for ( int i = 0 ; i <m; i ++) {
-            dp[i][0] = 1;
-        }
-
-        // Initialize dp, 1 xn whole situation is 1 
-        for ( int j = 0 ; j <n; j ++) {
-            dp[0][j] = 1;
-        }
-
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-
-        return dp[m - 1][n - 1];
-    }
-	 
-	 
-	 /*
-	 A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
-
-	 The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
-
-	 How many possible unique paths are there?
-
-
-
-	 */
-	  
-	 // 1D DP
-	
-	     public static int uniquePaths1(int m, int n) {
-	         if (m<0||n<0){
-	             return 0;
-	         }
-	         if (m==0 || n==0 ){
-	             return 1;
-	         }
-	         
-	         int[] arr=new int[n];
-	         arr[0]=1;
-	         
-	         for (int i=0; i<m; i++){
-	             for (int j=1; j<n; j++){
-	                 arr[j]=arr[j]+arr[j-1];
-	             }
-	         }
-	         
-	         return arr[n-1];
-	         
-	         
-	     
-	 }
-	 // 2D DP
-	
-	     public static int uniquePaths2(int m, int n) {
-	         if (m<0||n<0){
-	             return 0;
-	         }
-	         if (m==0 || n==0 ){
-	             return 1;
-	         }
-	         
-	         int[][] matrix=new int[m][n];
-	         for (int i=0; i<m; i++){
-	             matrix[i][0]=1;
-	         }
-	         for (int i=0; i<n; i++){
-	             matrix[0][i]=1;
-	         }
-	         
-	         for (int i=1; i<m; i++){
-	             for (int j=1; j<n; j++){
-	                 matrix[i][j]=matrix[i-1][j]+matrix[i][j-1];        
-	             }
-	         }
-	         
-	         return matrix[m-1][n-1];
-	         
-	     }
-	 
 }
--- src/Robot/UniqueSteps.java
+++ src/Robot/UniqueSteps.java
@@ -1,8 +1,18 @@
 
 /*
  * 
+Question: 
+A robot has to move in a grid which is in the form of a matrix. It can go to 
+1.) A(i,j)--> A(i+1,j) (Down) 
+2.) A(i,j)--> A(i,i+1) (Right)
 
-Explanation Source: 
+VERY VERY IMP NOTE: This question is different from minSteps question in the sense that 
+  * in that question the robot can move (i+j,j) and (i,i+j) BUT here it is (i+1,j) and (i,j+1)
+ 
+Find the number of uniue paths in which the robot can reach destination (m,n) from (1,1) 
+
+Question Source: http://www.careercup.com/question?id=5697293959299072
+Solution Source: 
 http://rleetcode.blogspot.com/2014/06/unique-paths-java.html
 http://articles.leetcode.com/2010/11/unique-paths.html
  */
@@ -10,5 +20,66 @@ http://articles.leetcode.com/2010/11/unique-paths.html
 package Robot;
 
 public class UniqueSteps {
+	public static void main(String[] args) {
+		System.out.println("The number of UNIQUE PATHS in which the robot can reach destination location (m,n)");
+		System.out.println(uniquePaths1(5, 8));
+		System.out.println(uniquePaths2(5, 8));
+	}
+	 
+	 /*
+	 A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 
+	 The robot can only move either down or right at any point in time. 
+	 The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+	 How many possible unique paths are there?
+	 */
+	  
+	 // 1D DP
+	
+	     public static int uniquePaths1(int m, int n) {
+	         if (m<0||n<0){
+	             return 0;
+	         }
+	         if (m==0 || n==0 ){
+	             return 1;
+	         }
+	         
+	         int[] arr=new int[n];
+	         arr[0]=1;                      // VERY IMP
+	         
+	         for (int i=0; i<m; i++)
+	             for (int j=1; j<n; j++)                // VERY IMP
+	                 arr[j]=arr[j]+arr[j-1];
+
+	         return arr[n-1];
+	 }
+	 // 2D DP
+	
+	     public static int uniquePaths2(int m, int n) {
+	         if (m<0||n<0){
+	             return 0;
+	         }
+	         if (m==0 || n==0 ){
+	             return 1;
+	         }
+	         
+	         int[][] matrix=new int[m][n];
+	         for (int i=0; i<m; i++){			// VERY IMP
+	             matrix[i][0]=1;
+	         }
+	         for (int i=0; i<n; i++){
+	             matrix[0][i]=1;
+	         }
+	         
+	         for (int i=1; i<m; i++){
+	             for (int j=1; j<n; j++){
+	                 matrix[i][j]=matrix[i-1][j]+matrix[i][j-1];        
+	             }
+	         }
+	         
+	         return matrix[m-1][n-1];
+	         
+	     }
+	 
 }
