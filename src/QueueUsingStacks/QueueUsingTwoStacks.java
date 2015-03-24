5923ff28bb0cecec496417d6298372c85c8f9a5b
Nikhil Katre <nkatre@ncsu.edu>
2015-03-24 19:17:41 -0400

    added content

--- src/QueueUsingStacks/QueueUsingTwoStacks.java
+++ src/QueueUsingStacks/QueueUsingTwoStacks.java
@@ -8,6 +8,19 @@
  * Part I. https://www.youtube.com/watch?v=_PIRZqC0pS0    (10 minutes)
  * Part II. https://www.youtube.com/watch?v=D2Z2iGQW7cM   (10 minutes)
  * Part III. https://www.youtube.com/watch?v=DeChmB6JSZw  (10 minutes)
+ * 
+ * Explanation:
+ * 1. We will use two stacks: oldStack and newStack
+ * 2. Whenever there is enqueue operation we will enter the value in newStack
+ * 3. Whenever there is dequeue we will check the following:
+ *     3.a. if oldStack is empty then we will pop all the elements from the newStack and insert into oldStack. 
+ *     Now we will pop an element from oldStack and return that value
+ *     3.b. if oldStack is not empty then we will simply pop an element from the oldStack and return it
+ * 4. To check the emptiness of the Queue, we need to check both oldStack and newStack. If both oldStack and newStack
+ * 	  are empty then the Queue is empty otherwise the queue is not empty
+ * 5. Similarly to check the size of the Queue we need to check both oldStack and newStack. The size of the Queue
+ * 	  is the sum of sizes of oldStack and newStack
+ * 
  */
 
 package QueueUsingStacks;
@@ -20,11 +33,11 @@ public class QueueUsingTwoStacks {
 		System.out.println("The queue is empty: "+q.isEmpty());
 		for(int i=0;i<5;i++){
 			q.enqueue(i*10);
-			System.out.println("Enqueued "+i*10+" into queue");
+			System.out.println("ENQUEUED "+i*10+" into queue");
 		}
 		int size = q.size();
 		for(int i=0;i<size;i++)
-			System.out.println("Dequeued "+q.dequeue()+" from the queue");
+			System.out.println("DEQUEUED "+q.dequeue()+" from the queue");
 	}
 	
 }
@@ -55,14 +68,12 @@ class Queue{
 		topElement=-1;
 		if(oldStack.empty()){
 			while(!newStack.empty()){
-				topElement=newStack.peek();
+				topElement=newStack.pop();
 				oldStack.push(topElement);
-				newStack.pop();
 			}
 		}
 		if(!oldStack.empty()){
-			topElement=oldStack.peek();
-			oldStack.pop();
+			topElement=oldStack.pop();
 		}
 		return topElement;
 	}
