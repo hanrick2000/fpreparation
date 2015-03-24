2d61998b2a5c5bb524fe3b9d17b8da05f959ef65
Nikhil Katre <nkatre@ncsu.edu>
2015-03-23 21:56:21 -0400

    added content

--- src/Othello/Question.java
+++ src/Othello/Question.java
@@ -1,7 +1,14 @@
 
 /*
  * Question:
- * Othello is played as follows: Each Othello piece is white on one side and black on the other. When a piece is surrounded by its opponents on both the left and right sides, or both the top and bottom, it is said to be captured and its color is flipped. On your turn, you must capture at least one of your opponent’s pieces. The game ends when either user has no more valid moves, and the win is assigned to the person with the most pieces. Implement the object oriented design for Othello. 
+ * Othello is played as follows: Each Othello piece is white on one side and black on the other. 
+ * When a piece is surrounded by its opponents on both the left and right sides, or both the top and bottom, 
+ * it is said to be captured and its color is flipped.
+ * On your turn, you must capture at least one of your opponent’s pieces. The game ends when either user has
+ * no more valid moves, and the win is assigned to the person with the most pieces. 
+ * Implement the object oriented design for Othello.
+ * 
+ *  
 SOLUTION 
 Othello has these major steps: 
 2.     Game () which would be the main function to manage all the activity in the game: 
