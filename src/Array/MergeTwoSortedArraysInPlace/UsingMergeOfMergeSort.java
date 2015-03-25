aa3b9fc8fb6e6b86b97d915baade9f47094ed149
Nikhil Katre <nkatre@ncsu.edu>
2015-03-25 12:32:03 -0400

    added content

--- src/Array/MergeTwoSortedArraysInPlace/UsingMergeOfMergeSort.java
+++ src/Array/MergeTwoSortedArraysInPlace/UsingMergeOfMergeSort.java
@@ -12,23 +12,15 @@ Algorithm:
 	The idea is merging from the back of the b array so the write op of the merge is 
 	never interfered with read op from b
 	
-*/	
 	
-package Array.MergeTwoSortedArraysInPlace;
-
-public class UsingMergeOfMergeSort {
-	public static void main(String[] args) {
-		
-	}
-	public static int[] merge(int[] a, int[] b){
-		/*
-		 * Read the question and implement the solution yourself. This is trivial hence I am not
-		 * implementing it right now
+	/*  IMP NOTE:
+		// 1. ASSUME THAT ARRAY 'B' = LENGTH OF ARRAY A + NO OF ELEMENTS IN ARRAY B
+		// 2. ALSO ASSUME THAT ARRAY B IS FILLED IN FRONT AND THE LAST POSITIONS OF ARRAY B ARE EMPTY
 		 * 
 		Algorithm:
 		int i_a = a.size() - 1;
-		int i_b = b.size() - 1;
-		int i_AB = a.size() + b.size() - 1;
+		int i_b = noOfElementsInArrayB-1;   // since array b is filled in front and the last positions of array b are empty
+		int i_AB = a.length+noOfElementsInArrayB-1;
 			
 		while(i_a >= 0 && i_b >=0)
 		{
@@ -43,13 +35,77 @@ public class UsingMergeOfMergeSort {
 		        b[i_AB--] = a[i_a--]; 
 		
 		
-		else  
-		{ 
-			// There is nothing to do if i_b > 0 as we merge onto b
+		while(i_b>=0){
+			// There is nothing to do if i_b >= 0 as we merge onto b
 		}
 		
-		*/
-		return null;
+		return b;
+*/	
+	
+package Array.MergeTwoSortedArraysInPlace;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class UsingMergeOfMergeSort {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		try{
+			System.out.println("Enter the number of elements of the first array");
+			int n = in.nextInt();
+			int[] a= new int[n];
+			System.out.println("Enter the elements of the first array");
+			for(int i=0;i<n;i++)
+				a[i]=in.nextInt();
+			System.out.println("Enter the number of elements of the second array");
+			System.out.println("THE SIZE OF B SHOULD BE ABLE TO INCORPORATE THE SIZE OF A AS WELL");
+			int m = in.nextInt();
+			int[] b = new int[m];
+			System.out.println("Enter the elements to be entered in b");
+			int noOfElementsInArrayB = in.nextInt();
+			System.out.println("Enter the elements of the second array");
+			System.out.println("FILLING THE ELEMENTS IN ARRAY B FROM THE FRONT AND LEAVING THE BACK EMPTY");
+			for(int i=0;i<noOfElementsInArrayB;i++)
+				b[i]=in.nextInt();
+			System.out.println("Inplace merge result is: "+Arrays.toString(merge(a,b,noOfElementsInArrayB)));
+		}
+		finally{
+			in.close();
+		}
+	}
+	public static int[] merge(int[] a, int[] b, int noOfElementsInArrayB){
+		
+		if(a==null && b==null)
+			return null;
+		
+		if(a==null)
+			return b;
+		
+		if(b==null)
+			return a;
+		
+		// assume that array 'b' has 2*length allocated space to merge in place
+		// also assume that array b is filled in front and the last positions of array b are empty
+		int iteratorA = a.length-1;
+		int iteratorB = noOfElementsInArrayB-1;
+		int iteratorAB = a.length+noOfElementsInArrayB-1;
+		
+		
+		while(iteratorA>=0 && iteratorB>=0){
+			if( a[iteratorA] > b[iteratorB] )
+				b[iteratorAB--]=a[iteratorA--];
+			else
+				b[iteratorAB--]=b[iteratorB--];
+		}
+		
+		while(iteratorA>=0)
+			b[iteratorAB--]=a[iteratorA--];
+		
+		while(iteratorB>=0){
+			// There is nothing to do if iteratorB >= 0 as we merge onto b
+		}
+		
+		return b;
 	}
 	/*
 	 * Analysis:
