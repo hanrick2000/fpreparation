4f70f05656f5e360fbc3505077aecbd1f2e21ab1
Nikhil Katre <nkatre@ncsu.edu>
2015-03-26 10:46:19 -0400

    added content

---
+++ src/Matrix/SortElementsFromRowSortedMatrix/SortElementsFromRowSortedMatrix.java
@@ -0,0 +1,83 @@
+
+/*
+Question:
+Given an m x n matrix where each row element is sorted, but the columns do not appear in sorted order, 
+write a function to print each matrix element in sorted order. 
+
+Example matrix: 
+matrix = [ 
+[20, 40, 80], 
+[5, 60, 90], 
+[45, 50, 55] 
+] 
+
+Your function should print 5, 20, 40, 45, 50, 55, 60, 80, 90. 
+
+Add on: Assume that we are space-constrained such that we can only hold one column in memory at a time. 
+Optimize your function to work under such constraints as efficiently as possible.
+
+Question and Answer Source: http://www.careercup.com/page?pid=facebook-interview-questions
+*/
+
+package Matrix.SortElementsFromRowSortedMatrix;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class SortElementsFromRowSortedMatrix {
+	public static void main(String[] args) {
+		int[][] matrix = new int[][]{{20, 40, 80}, {5, 60, 90}, {45, 50, 55}}; // row elements are already sorted
+		List<Integer> result = printSortedElements(matrix);		
+		System.out.println(result.toString());
+	}
+	public static List<Integer> printSortedElements(int[][] matrix){
+		
+		PriorityQueue<MatrixElement> minHeap = new PriorityQueue<MatrixElement>(matrix.length, new Comparator<MatrixElement>(){
+			public int compare(MatrixElement a, MatrixElement b){
+				return (a.value-b.value);
+			}
+		});
+		/*
+		PriorityQueue(int initialCapacity, Comparator)
+		Creates a PriorityQueue with the specified initial capacity that orders its elements according to 
+		their natural ordering.
+		 */
+		
+		List<Integer> result = new ArrayList<Integer>();
+		
+		for(int i=0;i<matrix.length;i++){
+			minHeap.add(new MatrixElement(matrix[i][0], i, 0));
+		}
+		
+		while(!minHeap.isEmpty()){
+			MatrixElement lowest = minHeap.remove();
+			
+			result.add(lowest.value);
+			
+			int nextRow = lowest.row;      // row will be the same of the next Element
+			int nextColumn = lowest.column+1; // we will get the next matrix element from the previousColumn+1
+			
+			if(nextColumn<matrix[lowest.row].length)                  // if the nextColumn is not out of bounds then add the element
+				minHeap.add(new MatrixElement(matrix[nextRow][nextColumn], nextRow, nextColumn));
+			
+		}
+		return result;
+	}
+	/*
+	 * Analysis:
+	 * Time Complexity = O(m*n)
+	 * Space Complexity = O(m*n) used by ArrayList to store the result
+	 */
+}
+class MatrixElement{
+	int value;
+	int row;
+	int column;
+	public MatrixElement(int value, int row, int column){
+		this.value=value;
+		this.row = row;
+		this.column = column;
+	}
+}
--- src/Testing/StaticVSNonStatic.java
+++ src/Testing/StaticVSNonStatic.java
@@ -15,6 +15,10 @@ public class StaticVSNonStatic {
 		demo.nonStaticSwap(a,1,8);
 		System.out.println(Arrays.toString(a));
 		
+		int[][] matrix = new int[][]{{20, 40, 80, 90}, {5, 60, 90, 100}, {45, 50, 55, 65}};
+		System.out.println(matrix.length);
+		System.out.println(matrix[0].length);
+		
 	}
 	public static void staticSwap(int[] a, int to, int from){
 		a[to]=a[to]^a[from];
