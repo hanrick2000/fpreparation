521d32bd7c8aaae98531d13de45e950610454e9c
Nikhil Katre <nkatre@ncsu.edu>
2015-03-27 13:30:55 -0400

    added content

---
+++ src/Testing/RollDice.java
@@ -0,0 +1,17 @@
+package Testing;
+
+
+/*
+ * Question: Roll two dice, what is the probability of rolling no sixes?
+ * 
+ * Question and Answer Source: http://www.careercup.com/question?id=5090125614678016
+ * 
+ * Explanation:
+ * Events that should happen are (i) dice A is not 6, (ii) dice B is not six,
+ *  both at the same time, both events are independent. 
+ *  Hence P = P_A*P_B = 5/6*5/6=25/36~= 0.69
+ */
+
+public class RollDice {
+
+}
