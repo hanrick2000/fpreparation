1160e11e90ea3ba8d92ff9b67af72573ed542ddc
Nikhil Katre <nkatre@ncsu.edu>
2015-03-27 15:28:22 -0400

    added content

---
+++ src/Array/AgeCount/AgeCountFromSortedArray.java
@@ -0,0 +1,45 @@
+package Array.AgeCount;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class AgeCountFromSortedArray {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		try{
+			System.out.println("Enter the number of elements of the sorted array");
+			int n = in.nextInt();
+			int[] input = new int[n];
+			System.out.println("Enter the elements");
+			for(int i=0;i<n;i++)
+				input[i]=in.nextInt();
+			System.out.println(count(input).toString());
+		}
+		finally{
+			in.close();
+		}
+	}
+	public static HashMap<Integer,Integer> count(int[] input) {
+		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+		count (input, 0, input.length-1, map);
+		return map;
+	}
+		
+	private static void count(int[] input, int begin, int end, HashMap<Integer,Integer> map) {
+		if (input[begin] == input[end]) {
+			if(map.get(input[begin])==null)
+				map.put(input[begin],end-begin+1);
+			else
+				map.put(input[begin],map.get(input[begin])+(end-begin+1));
+		}
+		else {
+			count(input, begin, (begin+end)/2, map);
+			count(input, (begin+end)/2 + 1, end, map);
+		}		
+	}
+	/*
+	 * Analysis:
+	 * Time Complexity = O(mlgn) where m = unique number of ages and n = total number of elements
+	 * Space Complexity = O(m) where m = unique number of ages
+	 */
+}
