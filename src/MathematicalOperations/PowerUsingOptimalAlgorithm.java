115b6f19f4a8e0b667d8c46f102322afbe563a31
Nikhil Katre <nkatre@ncsu.edu>
2015-03-30 12:48:50 -0400

    commit

--- src/MathematicalOperations/PowerUsingOptimalAlgorithm.java
+++ src/MathematicalOperations/PowerUsingOptimalAlgorithm.java
@@ -1,45 +1,60 @@
+
+/*
+ * Question: Implement EFFICIENT power method
+ * 
+ * Question and Answer Source: http://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
+ * 
+ */
+
+
 package MathematicalOperations;
 
 import java.util.Scanner;
 
 public class PowerUsingOptimalAlgorithm {
+	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		try{
-			System.out.println("Enter the two number to calculate the power in the form a^b repectively");
-			System.out.println("Only positive numbers should be entered");
-			int a = in.nextInt();
-			int b = in.nextInt();
-			System.out.println("Using multiplcative recursion we get "+pow(a,b));
-			
-			/*
-			 * pow function using % and / operators
-			 * 
-			 */
-		}
-		finally{
-			in.close();
-		}
+	Scanner in = new Scanner(System.in);
+	try{
+		System.out.println("lgn algorithm to calculate power");
+		System.out.println("Enter the two number to calculate the power in the form a^b repectively");
+		System.out.println("Works for both positive and negative numbers of a and b");
+		int a = in.nextInt();
+		int b = in.nextInt();
+		System.out.println("The result is: "+pow(a,b));
 	}
-
-	private static int pow(int a, int b) {
-		
-		if(b==0)
-			return 1;
-		int temp = pow(a,b>>1);         // (b>>1)  is  (b/2)
-		
-		if(b%2==0)
-			return temp*temp;
-		else{
-			if(b>0)
-				return (temp*temp*a);
-			else
-				return (temp*temp/a);
-		}
+	finally{
+		in.close();
 	}
 }
-/*
- * Analysis:
- * Time Complexity = O(lgn)
- * Space Complexity = O(1)
- */
+	
+	
+	public static double pow(double x, int n) {
+		if (n < 0) 
+			return 1 / power(x, -n);
+		else 
+			return power(x, n);
+	}
+	
+	public static double power(double x, int n) {
+		if (n == 0)
+			return 1;
+	 
+		double v = power(x, n / 2);
+	 
+		if (n % 2 == 0) 
+			return v * v;
+		else 
+			return v * v * x;
+	}
+	 
+
+
+
+	/*
+	 * Analysis:
+	 * Time Complexity = Since we divide b into 2 parts and discard one part,
+	 * the time complexity = O(lgn)
+	 * Space Complexity = O(1)
+	 */
+}
