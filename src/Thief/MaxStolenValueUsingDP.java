140072160365d9fb8ecb71d7c3b47f91ead782aa
Nikhil Katre <nkatre@ncsu.edu>
2015-03-29 21:43:03 -0400

    added content

--- src/Thief/MaxStolenValueUsingDP.java
+++ src/Thief/MaxStolenValueUsingDP.java
@@ -7,7 +7,7 @@
  * Question Source: http://codercareer.blogspot.com/2013/02/no-44-maximal-stolen-values.html
  * 
  * Example: 
- * int[] value={6,1,3,7,2};
+ * int[] values={6,1,3,7,2};
  * Output: 13
  */
 
@@ -15,30 +15,30 @@ package Thief;
 
 public class MaxStolenValueUsingDP {
 	public static void main(String[] args) {
-		int[] value={6,1,3,7,2};
-		int[] table=sol(value);
-		System.out.println(table[value.length-1]);
+		int[] values={6,1,3,7,2};
+		int[] table=sol(values);
+		System.out.println("Maximum value that can be stolen is: "+table[values.length-1]);
 	}
-	public static int[] sol(int[] value){
-		int[] table = new int[value.length];
-		int len=value.length;	
+	public static int[] sol(int[] values){
+		int[] table = new int[values.length];
+		int len=values.length;	
 		//initialization
-		table[0]=value[0];
+		table[0]=values[0];
 		if(len==1) 
 			return table;
 
-		if(value[0]>value[1])
-			table[1]=value[0];
+		if(values[0]>values[1])
+			table[1]=values[0];
 		else
-			table[1]=value[1];
+			table[1]=values[1];
 
 		if(len==2) 
 			return table;
 		
 		if(len>2){
 			for(int i=2; i<len; i++){
-				if( (value[i]+table[i-2]) > table[i-1]){
-					table[i]=value[i]+table[i-2];
+				if( (values[i]+table[i-2]) > table[i-1]){       // thief can take values from non-adjacent houses
+					table[i]=values[i]+table[i-2];
 				}
 				else
 					table[i]=table[i-1];
