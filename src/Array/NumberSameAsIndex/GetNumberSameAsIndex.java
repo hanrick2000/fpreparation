49b3b40a17e266945b4f4981537b8e12d3617363
Nikhil Katre <nkatre@ncsu.edu>
2015-03-30 04:10:18 -0400

    commit

--- src/Array/NumberSameAsIndex/GetNumberSameAsIndex.java
+++ src/Array/NumberSameAsIndex/GetNumberSameAsIndex.java
@@ -3,6 +3,19 @@
  * integer from the array who equals to its index. 
  * Example: In the array {-3, -1, 1, 3, 5}, the number 3 equals its index 3.
  * Question and Answer Source: http://codercareer.blogspot.in/
+ * 
+ * Algorithm:
+ * 	If we scan all integers in the array from beginning to end, we may check whether every element equals
+ *  its index. Obviously, this solution costs O(n) time.
+	Since numbers are sorted in the array, let's try to utilize the binary search algorithm to optimize.
+ 	Supposing we reach the ith element in the array at some step. If the value of element is also i, 
+ 	it is a target integer and let's return it.
+	What would happen when the value m is greater than the index i? For any k (k>0), the value of 
+	element with index i+k should be greater than or equal to m+k, because integers are unique and 
+	increasingly sorted in the array. Additionally because m>k, m+k>i+k. Therefore, every element on 
+	the right side of index i should be greater than its index in such a case.
+	Similarly, when the value of element with index i is less than i, every integer on the left side
+ 	should be less than its index. Please prove it if you are interested.
  */
 
 package Array.NumberSameAsIndex;
