5b0d7d65f4b4303b4e0913b94222f27842fc1e12
Nikhil Katre <nkatre@ncsu.edu>
2015-03-29 22:05:57 -0400

    added content

--- src/CuttingRod/CuttingRodProblem.java
+++ src/CuttingRod/CuttingRodProblem.java
@@ -5,9 +5,16 @@
  * Question and Answer Source: http://www.geeksforgeeks.org/dynamic-programming-set-13-cutting-a-rod/
  * 							   https://www.youtube.com/watch?v=U-09Gs6cbsQ&list=PL962BEE1A26238CA3&index=4
  * 
+For example, if length of the rod is 8 and the values of different pieces are given as following, 
+then the maximum obtainable value is 22 (by cutting in two pieces of lengths 2 and 6)
+
+
+length   | 1   2   3   4   5   6   7   8  
+--------------------------------------------
+price    | 1   5   8   9  10  17  17  20
  */
 
-package CuttingRodProblem;
+package CuttingRod;
 
 import java.util.Scanner;
 
