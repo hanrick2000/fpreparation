333f261c074a119891bfc2faa869506e869c4dc8
Nikhil Katre <nkatre@ncsu.edu>
2015-04-16 22:05:14 -0400

    added content

--- src/SqrtCbrtLogSquareCube/IterativeAndRecursiveCube.java
+++ src/SqrtCbrtLogSquareCube/IterativeAndRecursiveCube.java
@@ -1,3 +1,8 @@
+/*
+ * Question: Find the cube of a number without using * sign. Algorithm should be in O(lgn) time
+ * 
+ */
+
 package SqrtCbrtLogSquareCube;
 
 import java.util.Scanner;
@@ -16,24 +21,33 @@ public class IterativeAndRecursiveCube {
 		}
 	}
 public static int getCubeIteratively(int value) {
-	int multiplier = value;          
+	int multiplier1 = value;
+	int multiplier2 = value;
 	int original = value;            // store the original value
 	boolean odd = value % 2 != 0;
 	
-	while (multiplier > 1) {
-		value = value<<2;
-		multiplier = multiplier/2;
+	while (multiplier1 > 1) {
+		value = value<<1;
+		multiplier1 = multiplier1/2;
 	}
 
+	value = odd ? value + original : value;
+	original = value;                        // original is now changed
+	
+	while (multiplier2 > 1) {
+		value = value<<1;
+		multiplier2 = multiplier2/2;
+	}
 	return odd ? value + original : value;
 }
 /*
  * Analysis:
- * Time Complexity = O(lgn)
+ * Time Complexity = O(2 * lgn)
  * Space Complexity = O(1)
  */
 public static int getCubeRecursively(int value) {
-	return cube(value, value);
+	int newValue = cube(value, value);
+	return cube(newValue,value);
 }
 
 public static int cube(int value, int multiplier) {
@@ -43,13 +57,13 @@ public static int cube(int value, int multiplier) {
 	}
 	
 	if (multiplier % 2 == 0) 
-		return cube (value << 2, multiplier / 2);
-	
-	return cube(value << 2, multiplier / 2) + value;
+		return cube (value << 1, multiplier / 2);
+	else
+		return cube(value << 1, multiplier / 2) + value;
 }
 /*
  * Analysis:
- * Time Complexity = O(lgn)
+ * Time Complexity = O(2 * lgn)
  * Space Complexity = O(1)
  */
 }
