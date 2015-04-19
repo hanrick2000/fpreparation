432aceabea063ed615a75e4666013d5c7ebdc442
Nikhil Katre <nkatre@ncsu.edu>
2015-04-19 15:15:59 -0400

    added

---
+++ src/BST/ConnectRightNodesAtSameLevel/ConnectRightNodes.java
@@ -0,0 +1,41 @@
+/*
+Question:
+	Write a program to connect all the right nodes at the same level in a BST
+Question Source: http://www.careercup.com/question?id=5678808906596352
+Answer Source: http://www.geeksforgeeks.org/connect-nodes-at-same-level/
+
+Example:
+Write a function to connect all the adjacent nodes at the same level in a binary tree. 
+Structure of the given Binary Tree node is like following.
+
+struct node{
+  int data;
+  struct node* left;
+  struct node* right;
+  struct node* nextRight;  
+}
+Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to 
+point next right for each node.
+
+Example
+
+Input Tree
+       A
+      / \
+     B   C
+    / \   \
+   D   E   F
+
+
+Output Tree
+       A--->NULL
+      / \
+     B-->C-->NULL
+    / \   \
+   D-->E-->F-->NULL
+*/	
+package BST.ConnectRightNodesAtSameLevel;
+
+public class ConnectRightNodes {
+	
+}
